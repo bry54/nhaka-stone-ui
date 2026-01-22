49891247f06ed01c9b6ebb1b574b4ef08f167789
Brian Paidamoyo Sithole <brianacyth@icloud.com>
2026-01-22 22:28:53 +0200

    Updated env variables

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig, AxiosError } from 'axios';
 import { AUTH_LOCAL_STORAGE_KEY } from '@/auth/lib/helpers';
 
-const API_URL = 'http://localhost:3000/api/v1';
+const API_URL = `${import.meta.env.VITE_API_URL}` || 'http://localhost:3000/api/v1';
 
 const api = axios.create({
     baseURL: API_URL,
